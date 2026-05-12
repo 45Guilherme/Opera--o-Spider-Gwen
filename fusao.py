dc0cfba7f7b08f6175fa86912e869d4e261756da
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2026-05-12 06:57:35 -0300

    2 - melhoramento do if

--- fusao.py
+++ fusao.py
@@ -10,5 +10,5 @@ imagens_extraidas = []
 with rarfile.RarFile("Homem-aranha Noir -2008.cbr") as gibi:
     imagens = gibi.namelist()
     for imagem in imagens:
-       if imagem.endswith(".jpg") or imagem.endswith(".png"):
+       if imagem.lower().endswith((".jpg", ".png", ".jpeg")):
            imagens_extraidas.append(imagem)
