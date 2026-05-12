60abca049b153cc5d1c6b12a0786b70bc7c1831e
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2026-05-12 06:54:18 -0300

    Update fusao.py

--- fusao.py
+++ fusao.py
@@ -1,4 +1,14 @@
 
 
-imagens = gibi.namelist()
+import rarfile
 
+rarfile.UNRAR_TOOL = "UnRAR.exe"
+
+imagens_extraidas = []
+
+
+with rarfile.RarFile("Homem-aranha Noir -2008.cbr") as gibi:
+    imagens = gibi.namelist()
+    for imagem in imagens:
+       if imagem.endswith(".jpg") or imagem.endswith(".png"):
+           imagens_extraidas.append(imagem)
